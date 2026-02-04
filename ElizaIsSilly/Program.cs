5045b57346fc141400ca24953b45e8902cb2c4be
Mansour Emami <mansuurem@gmail.com>
2026-02-04 12:31:30 +0100

    Doctor case

---
+++ ElizaIsSilly/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ElizaIsSilly
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Doctor.Intro());
+
+            var userInput = "";
+            userInput = Console.ReadLine();
+
+
+            while (userInput.Equals("quit", StringComparison.OrdinalIgnoreCase))
+            {
+                
+                userInput = Console.ReadLine();
+            }
+        }
+    }
+}
