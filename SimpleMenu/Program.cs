465b6a765d1db64e547ad8e640c46021102cf6a9
Mansour Emami <mansuurem@gmail.com>
2026-02-05 15:38:39 +0100

    keyChar implementation

---
+++ SimpleMenu/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleMenu
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Please choose one of the following options: ");
+            Console.WriteLine("1 - Cappuccino");
+            Console.WriteLine("2 - Latte");
+            Console.WriteLine("3 - Espresso");
+            Console.WriteLine("4 - Americano");
+            Console.WriteLine("5 - Mocha");
+            Console.WriteLine("6 - Macchiato");
+            Console.WriteLine("Q - Quit");
+
+            ConsoleKeyInfo userChoice = Console.ReadKey(true);
+            Console.WriteLine("You chose: " + userChoice);
+
+            var x = userChoice.Key;
+            char y = userChoice.KeyChar;
+            var z = userChoice.Modifiers;
+            Console.WriteLine("Key: " + x);
+            Console.WriteLine("KeyChar: " + y);
+            Console.WriteLine("keyModifier: " + z);
+
+        }
+    }
+}
